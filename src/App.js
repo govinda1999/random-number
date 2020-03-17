35b35e43e1f1ca6dd112a9866791a993165a08de
govinda1999 <gspatel61199@gmail.com>
2020-03-18 02:35:24 +0530

    done with logic part

--- src/App.js
+++ src/App.js
@@ -6,19 +6,47 @@ export default class App extends Component{
     super(props);
     this.state = {
       number:0,
-      value:999,
+      value:0,
+      color:""
     }
   }
-  componentWillMount  = () =>{
+
+  componentDidMount  = () =>{
     this.setState({
       number:Math.floor(Math.random()*101) 
-    });
+    },()=> console.log(this.state.number));
   }
+
+  handlechange = (e) =>{
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  
+  handlesubmit = () =>{
+    if(Math.abs(this.state.number-parseInt(this.state.value))===0){
+      this.setState({color:"green"});
+    }
+    else if(Math.abs(this.state.number-parseInt(this.state.value))<=4){
+      this.setState({color:"red"});
+    }
+    else if(Math.abs(this.state.number-parseInt(this.state.value))<=15){
+      this.setState({color:"yellow"});
+    }
+    else{
+      this.setState({color:"blue"});
+    }
+  }
+
   render(){
     return(
-      <div className="mt-5">
+      <div className="mt-5 container">
       <h1 className="text-center text-primary">Random Number Generator</h1>
-      <p>{this.state.number}</p>
+      <div className="row justify-content-center mt-5">
+      <div className="col-5">
+      <input className="w-100" type="number" value={this.state.value} onChange={this.handlechange} placeholder="Enter number" name="value"/>
+      <button className="w-100 btn btn-outline-primary mt-3" onClick={this.handlesubmit}>Submit</button>
+      <p>{this.state.color}</p>
+      </div>
+      </div>
       </div>
     );
   }
