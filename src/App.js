c42c68bfe69b3dec7dd58a48a93162abf8482b8d
govinda1999 <gspatel61199@gmail.com>
2020-03-18 02:02:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      number:0,
+      value:999,
+    }
+  }
+  componentWillMount  = () =>{
+    this.setState({
+      number:Math.floor(Math.random()*101) 
+    });
+  }
+  render(){
+    return(
+      <div className="mt-5">
+      <h1 className="text-center text-primary">Random Number Generator</h1>
+      <p>{this.state.number}</p>
+      </div>
+    );
+  }
 }
-
-export default App;
